0615719d382b101022bbd33196a592403fc2b85c
hocamole <32062712+hocamole@users.noreply.github.com>
2024-02-29 16:04:04 +0900

    Create is_comma_or.sv

---
+++ grammar check/is_comma_or.sv
@@ -0,0 +1,34 @@
+//If always uses two argument divided by comma, 
+//does it mean block will be executed when one of arguments is satisfied?
+`timescale 1ns / 100ps
+
+module test;
+  reg clk;
+  reg nclk;
+  reg[4:0] count;
+
+  initial begin
+    count = 0;
+    clk = 0;
+    nclk = 1;
+  end
+
+  always@(*) begin
+     #5 clk <= ~clk;
+     #5 nclk <= ~nclk;
+  end
+
+  always@ (posedge clk, negedge nclk) 
+    begin
+    if(count < 10) begin 
+      count <= count + 1; 
+    end  
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd"); 
+    $dumpvars;
+    # 120
+    $finish();
+  end 
+endmodule
