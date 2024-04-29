76b69ed6fc7e75a18c3e86a3389ec0a126973b32
hocamole <32062712+hocamole@users.noreply.github.com>
2024-04-29 14:31:50 +0900

    Update is_task_sequential.sv

--- grammar check/is_task_sequential.sv
+++ grammar check/is_task_sequential.sv
@@ -1,3 +1,6 @@
+// Are commands of task executed sequentially?
+// --> Yes. Unlike normal Verilog block, task acts like function of programming language.
+//          In other words, it executes its commands one by one.
 `timescale 1ns / 100ps
 
 class testc;
